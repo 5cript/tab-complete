de8babfa5d65d963ca0e450550ffb06eb1de4f04
5cript <tim06tr@gmail.com>
2020-02-26 16:36:13 +0100

    Added cmake file and fixed warnings in utf-8.hpp

--- utf-8.hpp
+++ utf-8.hpp
@@ -14,21 +14,21 @@ std::string codepageToUtf8(uint32_t index)
 
     if (index <= 0x7FF)
         return {
-            static_cast <char> (0b1100'0000) | static_cast <char> ((index >> 6) & 0x1F),
-            static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F)
+            static_cast <char> (static_cast <char> (0b1100'0000) | static_cast <char> ((index >> 6) & 0x1F)),
+            static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F))
         };
 
     if (index <= 0xFFFF)
         return {
-            static_cast <char> (0b1110'0000) | static_cast <char> ((index >> 12) & 0xF),
-            static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 6) & 0x3F),
-            static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F)
+            static_cast <char> (static_cast <char> (0b1110'0000) | static_cast <char> ((index >> 12) & 0xF)),
+            static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 6) & 0x3F)),
+            static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F))
         };
 
     return {
-        static_cast <char> (0b1111'0000) | static_cast <char> ((index >> 18) & 0x7),
-        static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 12) & 0x3F),
-        static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 6) & 0x3F),
-        static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F)
+        static_cast <char> (static_cast <char> (0b1111'0000) | static_cast <char> ((index >> 18) & 0x7)),
+        static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 12) & 0x3F)),
+        static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> ((index >> 6) & 0x3F)),
+        static_cast <char> (static_cast <char> (0b1000'0000) | static_cast <char> (index & 0x3F))
     };
 }
